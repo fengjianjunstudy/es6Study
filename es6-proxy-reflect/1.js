c4e1e705ae76b2865c9990ccf6a365031aff60e9
fjj <fjj_321@126.com>
2016-06-16 12:20:57 +0800

    proxy reflect

--- es6-proxy-reflect/1.js
+++ es6-proxy-reflect/1.js
@@ -1,3 +1,19 @@
 /**
  * Created by fengjj on 2016/5/27.
  */
+/*
+    Proxy  对外界对象的访问进行过滤和改写    代理器   目前只有Firefix 49+ 支持
+* */
+'use strict';
+let obj = new Proxy({},{
+    get:function(target,key,receiver) {
+        console.log('====get====');
+        return Reflect.get(target,key,receiver);
+    },
+    set: function (target,key,value,receiver) {
+        console.log('=====set====');
+        return Reflect.set(target,key,value,receiver);
+    }
+});
+obj.count = 1;
+console.log(obj.count);
