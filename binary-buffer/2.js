6a378f08ca99f4417330995884348d729c2ba5b8
fjj <fjj_321@126.com>
2016-06-24 17:46:55 +0800

    binaru-buffer

--- binary-buffer/2.js
+++ binary-buffer/2.js
@@ -2,26 +2,21 @@
  * Created by fengjj on 2016/6/23.
  */
 /*
-    ArrayBuffer  原始的二进制数据
-        不能直接读/写，只能通过视图（TypedArray 和 DataView）读/写
-        new ArrayBuffer(n)  分配一段存放数据的连续内存区域   注意：是连续的
-        参数n:所需内存的大小（单位：字节）
-    typedArray  操作简单类型的二进制数据
-        Int8 Uint8 Uint8C Int16 Uint16 Int32 Uint32 Float32 Float64
-    DataView    操作复杂的二进制数据
-        Int8 Uint8  Int16 Uint16 Int32 Uint32 Float32 Float64
-        new DataView(buffer)
-    用到二进制数据的操作：
-    File API
-    XMLHttpRequest
-    Fetch API
-    Canvas
-    WebSokets
+    ArrayBuffer实例属性及方法及静态方法
+
 * */
 'use strict';
-let buf = new ArrayBuffer(10);
-let dataView = new DataView(buf);
-dataView.setInt8(0,6);
-console.log(dataView.getInt16(0,true));
-console.log(buf.byteLength);
+let buffer = new ArrayBuffer(12);
+let int8_arr = new Int8Array(buffer);
+int8_arr.set([1,2,3]);
+//ArrayBuffer.prototype.byteLength
+console.log('== ArrayBuffer.prototype.byteLength ==',buffer.byteLength);
 
+// ArrayBuffer.prototype.slice(begin[,end])  返回新的ArrayBuffer对象  1/分配新的内容 2/将原来复制的ArrayBuffer对象复制过去
+let new_buf = buffer.slice(0,3);
+let new_int8_arr = new Int8Array(new_buf);
+console.log('== ArrayBuffer.prototype.slice(begin[,end]) ===',new_int8_arr);
+
+
+//ArrayBuffer.isView(arg) return Boolean  判断arg是否为ArrayBuffer视图
+console.log('== ArrayBuffer.isView(arg) ==',ArrayBuffer.isView(new_int8_arr));
--- binary-buffer/3.js
+++ binary-buffer/3.js
@@ -2,21 +2,58 @@
  * Created by fengjj on 2016/6/23.
  */
 /*
-    ArrayBuffer实例属性及方法及静态方法
+    视图：同一段内存，不同数据有不同的解读方式
+    TypedBuffer视图 9种类型（Int8 Uint8 Uint8C Int16 Uint16 Int32 Uint32 Float32 Float64）
+     new TypedArray(length);
+     new TypedArray(typedArray);
+     new TypedArray(arrayLikeObject);
+     new TypedArray(buffer [, byteOffset [, length]]);
 
+     where TypedArray() is one of:
+
+     Int8Array();
+     Uint8Array();
+     Uint8ClampedArray();
+     Int16Array();
+     Uint16Array();
+     Int32Array();
+     Uint32Array();
+     Float32Array();
+     Float64Array();
+
+    每个视图就像普通的数组，数组的所有方法(除concat)都可以用在上面
+    TypedBuffer数组：
+        所有的成员都是一个类型
+        连续的，不会有空位
+        数组成员默认值：0
 * */
 'use strict';
-let buffer = new ArrayBuffer(12);
-let int8_arr = new Int8Array(buffer);
-int8_arr.set([1,2,3]);
-//ArrayBuffer.prototype.byteLength
-console.log('== ArrayBuffer.prototype.byteLength ==',buffer.byteLength);
+// new TypedArray(length)
+let b = new Int16Array(8);   //可以不通过ArrayBuffer创建，可以直接分配内存  8  代表成员的个数，即8个2字节
+console.log('== new TypedArray(length) ==',b);
 
-// ArrayBuffer.prototype.slice(begin[,end])  返回新的ArrayBuffer对象  1/分配新的内容 2/将原来复制的ArrayBuffer对象复制过去
-let new_buf = buffer.slice(0,3);
-let new_int8_arr = new Int8Array(new_buf);
-console.log('== ArrayBuffer.prototype.slice(begin[,end]) ===',new_int8_arr);
+//new TypedArray(typedArray)
+let buf = new ArrayBuffer(8);
+let b1 = new Int32Array(buf);
+console.log('===new TypedArray(typedArray)=',b1);
+let b2 = new Int16Array(buf,2); //注意：16位  2个字节  此处的byteOffset必须是2的倍数
+b2.set([1,2],1);
+console.log(b2);
 
+//合并多个TypedArray数组
+function concatTypedArr(restutConstructor,...arrays) {
+    let totalLength = 0;
+    for(let arr of arrays) {
+        totalLength += arr.length;
+    }
+    let result = new restutConstructor(totalLength);
+    let offset = 0;
+    for(let arr of arrays) {
+        result.set(arr,offset);
+        offset += arr.length;
+    }
+    return result;
+}
+let concatArr = concatTypedArr(Int8Array,Int8Array.of(0,1),Int8Array.of(5,6));
+console.log(concatArr);
 
-//ArrayBuffer.isView(arg) return Boolean  判断arg是否为ArrayBuffer视图
-console.log('== ArrayBuffer.isView(arg) ==',ArrayBuffer.isView(new_int8_arr));
