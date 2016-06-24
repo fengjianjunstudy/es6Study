0c4a7a6c9314a336f48f46ad15071c52639c7958
fjj <fjj_321@126.com>
2016-06-24 17:47:18 +0800

    decorator

--- decorator/1.js
+++ decorator/1.js
@@ -1,3 +1,46 @@
 /**
  * Created by fengjj on 2016/6/24.
  */
+/*
+    修饰器（decorator）:修改类和方法的行为 (不确定的说法)是在编译时执行，而不是在运行时执行   直白的理解就是一个在编译时执行的函数
+    define function decoratorName(target,name,descriptor)
+    usage: @decoratorName
+    如果想要传一些参数的话，可以用Decorator Factories
+* */
+function testable(target) {
+    target.isTestable = true; // 为MyTest类添加一个静态属性
+}
+@testable
+class MyTest {
+}
+console.log(MyTest.isTestable);
+function Component(opts) {
+    return function(target,name,descriptor) {
+        target.selector = opts.selector;
+    }
+}
+@Component({selector:'my-app'})
+class AppComponent {
+    constructor(name = 'xiaoming') {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
+console.log(AppComponent.selector); //my-app
+
+function mixins(...list) {
+
+    return function (target,name,descriptor) {
+        Object.assign(target.prototype,...list);
+        console.log(Object.getOwnPropertyNames(target.prototype));
+    }
+}
+
+@mixins(AppComponent)
+class ABC {
+
+}
+let a = new ABC();
+console.log(a.selector);  // my-app
--- decorator/2.js
+++ decorator/2.js
@@ -1,3 +1,20 @@
 /**
  * Created by fengjj on 2016/6/24.
  */
+/*
+    方法修饰器
+* */
+function readOnly(target,name,descriptor) {
+    descriptor.writable = false;
+    return descriptor;
+}
+class A {
+    @readOnly
+    getName() {
+        console.log('xiaohua');
+    }
+}
+console.log(Object.getOwnPropertyNames(A.prototype))
+Object.defineProperty(A.prototype,'getName',function(){console.log('xiaoming')});
+let a = new A();
+console.log(a.getName());
