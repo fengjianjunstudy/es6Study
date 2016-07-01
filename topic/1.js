58114319e225e3154f762f26b071975ee221541b
fjj <fjj_321@126.com>
2016-07-01 16:25:16 +0800

    练习题

--- topic/1.js
+++ topic/1.js
@@ -1,3 +1,17 @@
 /**
  * Created by fengjj on 2016/7/1.
  */
+'use strict';
+let A = [6,3,9,3,2,4,5,7];
+let B = [5,8,6,2,1,9];
+let arr1 =Array.from(new Set(A));
+let arr2 = Array.from(new Set(B));
+let fn = (arr1,arr2) => {
+    return arr1.filter((v) => {
+        return !arr2.includes(v);
+    })
+}
+let r1 = fn(arr1,arr2),
+    r2 = fn(arr2,arr1);
+let result = [...r1,...r2];
+console.log(result);
--- topic/2.js
+++ topic/2.js
@@ -1,3 +1,24 @@
 /**
  * Created by fengjj on 2016/7/1.
  */
+Function.prototype.bind = function() {
+    var slice = Array.prototype.slice;
+    var self = this;
+    var thisObj = arguments[0];
+    var args = slice.call(arguments,1);
+    return function () {
+        var argsAll = args.concat(slice.call(arguments,0));
+        console.log(argsAll)
+        self.apply(thisObj,argsAll);
+    }
+}
+function fn(age,sex){
+    console.log(this.name);
+    console.log(age);
+    console.log(sex);
+}
+var obj = {
+    name:'xiaoming'
+}
+var fn1 = fn.bind(obj,18);
+fn1('girl');
