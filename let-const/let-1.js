4218c2e2bc696a6b5d202c8d3bec58cd7d123600
fjj_321 <fjj_321@126.com>
2016-07-07 15:08:12 +0800

    jjj

--- let-const/let-1.js
+++ let-const/let-1.js
@@ -6,10 +6,10 @@
 //暂时性死区
 //不允许重复声明
 
-var arr=[];
-for(let i=0;i<10;i++){
-    arr[i]=function(){
-        console.log(i);
-    }
-}
-arr[6]();
+//var arr=[];
+//for(let i=0;i<10;i++){
+//    arr[i]=function(){
+//        console.log(i);
+//    }
+//}
+//arr[6]();
--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-require("!style!css!./style.css") // 载入 style.css
-document.write('It works.')
-document.write(require('./mudule.js'))
+/*require("!style!css!./style.css") // 载入 style.css
+document.write('It works.')*/
+'use strict';
+let name = require('./moduler/nodeModule').name;
+console.log(name);  //xiaoming
+setTimeout(() => {
+    console.log(name); //xiaoming
+},1500);
